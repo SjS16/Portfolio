871cd154521c1a001edea9294a13620761c8e5ce
Steph Simpson <stephasimpson911@gmail.com>
2018-06-01 08:24:40 -0700

    added destroy action for portfoios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,4 +40,15 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    #Find the record
+    @portfolio_item = Portfolio.find(params[:id])
+    #Destroy the record
+    @portfolio_item.destroy
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully removed.' }
+    end
+  end
+
 end
