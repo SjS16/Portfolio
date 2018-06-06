fe333cb517b26f80d2c81613144caee7b19c2954
Steph Simpson <stephasimpson911@gmail.com>
2018-06-06 07:27:33 -0700

    implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'ruby-items', to: 'portfolios#ruby_on_rails'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
