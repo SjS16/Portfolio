5dcf8fd51e60964044095319f715b2a34cfc9e31
Steph Simpson <stephasimpson911@gmail.com>
2018-06-07 07:31:11 -0700

    implemented null object pattern to have greeting for guest user if theres no current user using openstruct

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  #moved to a module so that we aren't doing too many things in a controller where it shouldn't be
+  #moved to a concern module so that we aren't doing too many things in a controller where it shouldn't be
   include DeviseWhitelist
   include SetSource
+  include CurrentUserConcern
 
 end
