9c808bf9b2644703227c87804c2dc2c7c1a75125
Steph Simpson <stephasimpson911@gmail.com>
2018-06-14 07:51:45 -0700

    integrated initial sortable drag and drop functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require jquery-ui
-//= require activestorage
 //= require html.sortable
+//= require rails-ujs
+//= require activestorage
 //= require turbolinks
 //= require_tree .
 //= require jquery3
