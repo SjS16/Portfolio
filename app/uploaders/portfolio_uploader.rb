da96012ec90b0288061415d8677ba579fa22f9ea
Steph Simpson <stephasimpson911@gmail.com>
2018-06-19 20:03:36 -0700

    undid the aws section

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,6 +1,6 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
 
-  storage :aws
+  storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -9,5 +9,7 @@ class PortfolioUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg gif png)
   end
+
+
 
 end
